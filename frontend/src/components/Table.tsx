cb2d4a3a84aebf78529c5eaf6e5f90bc105c2807
Franziska Pendzialek <franziska.pendzialek@outlook.com>
2022-03-03 23:52:23 +0100

    007 add simple pagination

--- frontend/src/components/Table.tsx
+++ frontend/src/components/Table.tsx
@@ -1,93 +1,90 @@
 import React, { useState } from "react";
 import { ITableData, ITableRowData } from "../types/Table";
 import { TABLE_TITLES } from "../util/constants/table";
+import { compare } from "../util/helpers";
 import TableRow from "./TableRow";
 
 const Table = ({ tableRows }: ITableData) => {
   const [sortedField, setSortedField] = useState(" ");
+  const [currentPage, setCurrentPage] = useState(1);
+  const tableRowView = [[...tableRows].slice(0, 5), [...tableRows].slice(5, 10)];
 
-  const compare = (x: any,y: any) => {
-    if (x > y) {
-      return 1;
-    }
-    if (x < y) {
-      return -1;
-    }
-    return 0;
+  const handlePagination = (val: number) => {
+    setCurrentPage(val-1);
   }
 
-  function sortDataBy(data: ITableRowData[], byKey: string) {
-    let sortedData = [...tableRows] as any[];
+  const sortDataBy = (data: ITableRowData[], byKey: string) => {
+    let sortedData = [...tableRowView[currentPage]] as any[];
     if (byKey === "customer.lastName") {
       sortedData = data.sort(function (a, b) {
         let x = a.customer.lastName.toLowerCase();
         let y = b.customer.lastName.toLowerCase();
-        const result = compare(x,y);
-        return result
+        const result = compare(x, y);
+        return result;
       });
     }
     if (byKey === "customer.dateOfBirth") {
       sortedData = data.sort(function (a, b) {
         let x = a.customer.dateOfBirth;
         let y = b.customer.dateOfBirth;
-        const result = compare(x,y);
-        return result
+        const result = compare(x, y);
+        return result;
       });
     }
     if (byKey === "insurance") {
       sortedData = data.sort(function (a, b) {
         let x = a.insurance;
         let y = b.insurance;
-        const result = compare(x,y);
-        return result
+        const result = compare(x, y);
+        return result;
       });
     }
     if (byKey === "provider") {
       sortedData = data.sort(function (a, b) {
         let x = a.provider.toLowerCase();
         let y = b.provider.toLowerCase();
-        const result = compare(x,y);
-        return result
+        const result = compare(x, y);
+        return result;
       });
     }
     if (byKey === "policyNumber") {
       sortedData = data.sort(function (a, b) {
         let x = a.policyNumber.toLowerCase();
         let y = b.policyNumber.toLowerCase();
-        const result = compare(x,y);
-        return result
+        const result = compare(x, y);
+        return result;
       });
     }
     if (byKey === "status") {
       sortedData = data.sort(function (a, b) {
         let x = a.status;
         let y = b.status;
-        const result = compare(x,y);
-        return result
+        const result = compare(x, y);
+        return result;
       });
     }
     if (byKey === "createdAt") {
       sortedData = data.sort(function (a, b) {
         let x = a.createdAt;
         let y = b.createdAt;
-        const result = compare(x,y);
-        return result
+        const result = compare(x, y);
+        return result;
       });
     }
     if (byKey === "startDate") {
       sortedData = data.sort(function (a, b) {
         let x = a.startDate;
         let y = b.startDate;
-        const result = compare(x,y);
-        return result
+        const result = compare(x, y);
+        return result;
       });
     }
     if (byKey === "endDate") {
       sortedData = data.sort(function (a, b) {
         let x = a.endDate;
         let y = b.endDate;
-        const result = compare(x,y);
-        return result
+        const result = compare(x, y);
+        return result;
       });
     }
     return sortedData;
@@ -121,12 +118,16 @@ const Table = ({ tableRows }: ITableData) => {
               </tr>
             </thead>
             <tbody>
-              {sortDataBy([...tableRows], sortedField).map((item) => {
+              {sortDataBy([...tableRowView[currentPage]], sortedField).map((item) => {
                 return <TableRow key={item.id} tableRow={item} />;
               })}
             </tbody>
           </table>
         </div>
+      </div>
+      <div className="flex flex-row ml-10">
+        <button type="button" className={`pagination-box ${currentPage === 0 && 'pagination-box-active'}`} onClick={()=>{handlePagination(1)}}>1</button>
+        <button type="button" className={`pagination-box ${currentPage === 1 && 'pagination-box-active'}`} onClick={()=>{handlePagination(2)}}>2</button>
       </div>
     </div>
   );
---
+++ frontend/src/util/helpers.js
@@ -0,0 +1,9 @@
+export const compare = (x, y) => {
+  if (x > y) {
+    return 1;
+  }
+  if (x < y) {
+    return -1;
+  }
+  return 0;
+};
